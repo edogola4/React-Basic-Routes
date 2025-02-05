9bcd247bf133d3a71a5bfd23b3405d45a080d11f
edogola4 <edogola4@gmail.com>
2025-02-05 13:16:22 +0300

    ErrorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,20 +1,5 @@
-// src/pages/ErrorPage.js
-/*import React from 'react';
-import NavBar from '../components/NavBar';
-
-const ErrorPage = () => {
-  return (
-    <div>
-      <NavBar />
-      <h1>Oops! Looks like something went wrong.</h1>
-    </div>
-  );
-};
-
-export default ErrorPage;
-*/
-
-import NavBar from "../components/NavBar"; 
+// src/pages/ErrorPage.jsx
+import NavBar from "../components/NavBar";
 
 function ErrorPage() {
   return (

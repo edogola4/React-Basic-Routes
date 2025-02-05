c15a8c47bd723536a3620f42ba79300aa043ecb5
edogola4 <edogola4@gmail.com>
2025-02-05 12:44:22 +0300

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,50 @@
-import { useEffect, useState } from "react";
+/*import { useEffect, useState } from "react";
 
 function Home() {
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        {/* What component should go here? *}
       </header>
       <main>
-        {/* Info goes here! */}
+        {/* Info goes here! *}
       </main>
     </>
   );
 };
+
+export default Home;
+*/
+import { useEffect, useState } from "react";
+import NavBar from "./NavBar";      // Make sure the path is correct
+import MovieCard from "./MovieCard"; // Make sure the path is correct
+
+function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // Sample movie data; note that we include at least three movies.
+    const moviesData = [
+      { id: 1, title: "Doctor Strange" },
+      { id: 2, title: "Inception" },
+      { id: 3, title: "The Matrix" },
+    ];
+    setMovies(moviesData);
+  }, []);
+
+  return (
+    <>
+      <header>
+        <NavBar />
+        <h1>Home Page</h1>
+      </header>
+      <main>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </main>
+    </>
+  );
+}
 
 export default Home;

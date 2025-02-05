45d810d002b01f9a5bdce875396fcab8d660c959
edogola4 <edogola4@gmail.com>
2025-02-05 12:18:22 +0300

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,25 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+    <NavLink 
+    to='/'
+    end
+    className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+    >
+      Home
+    </NavLink>
+    <NavLink
+    to='/directors'
+    className={({ isActive }) => (isActive ? 'active' : '')}
+    >
+      Directors
+    </NavLink>
+    <NavLink
+    to='/actors'
+    className={({ isActive }) => (isActive ? 'active' : '')}
+    >
+      Actors
+    </NavLink>
     </nav>
     );
 };

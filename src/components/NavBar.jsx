8a16714b3714b75bc8ab6ce634675743ec535f8f
edogola4 <edogola4@gmail.com>
2025-02-05 13:21:23 +0300

    updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,28 @@
 // src/components/NavBar.jsx
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/directors">Directors</Link>
-      <Link to="/actors">Actors</Link>
+    <nav className="navbar" role="navigation">
+      <NavLink 
+        to="/" 
+        end
+        className={({ isActive }) => (isActive ? "active" : "")}
+      >
+        Home
+      </NavLink>
+      <NavLink 
+        to="/directors" 
+        className={({ isActive }) => (isActive ? "active" : "")}
+      >
+        Directors
+      </NavLink>
+      <NavLink 
+        to="/actors" 
+        className={({ isActive }) => (isActive ? "active" : "")}
+      >
+        Actors
+      </NavLink>
     </nav>
   );
 }

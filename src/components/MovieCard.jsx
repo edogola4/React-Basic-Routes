05cd5fbb3f9f62458dd4706cefc6a74658089f88
edogola4 <edogola4@gmail.com>
2025-02-05 12:32:57 +0300

    updated

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,19 @@
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function MovieCard({title}) {
+
+function MovieCard({title, id}) {
   
   return (
     <article>
         <h2>{title}</h2>
         {/* What should go here? */}
+        <Link to={`/movie/${id}`}>View Details</Link>
     </article>
   );
 };
-
+MovieCard.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+};
 export default MovieCard;

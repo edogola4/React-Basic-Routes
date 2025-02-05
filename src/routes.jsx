4b5a6b2f33f84b25f193dd3ab8bb509ce36f4933
edogola4 <edogola4@gmail.com>
2025-02-05 11:58:15 +0300

    updated

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,34 @@
-
+//import React from "react";
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import Directors from "./pages/Directors";  
+import Actors from "./pages/Actors";
+import ErrorPage from './pages/ErrorPage';
 
 const routes = [
-   
+   {
+    path: '/',
+    element: < Home />,
+    errorElement: < ErrorPage />
+   },
+   {
+    path: '/movie/:movieId',
+    element: < Movie />,
+    errorElement: < ErrorPage />
+
+   },
+   {
+    path: '/directors',
+    element: < Directors />,
+    errorElement: < ErrorPage />
+
+   },
+   {
+    path: '/actors',
+    element: < Actors />,
+    errorElement: < ErrorPage />
+
+   }
   ];
 
 export default routes;

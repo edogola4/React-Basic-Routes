db5a3939426ccad936cc9b152885420cfd0ff889
edogola4 <edogola4@gmail.com>
2025-02-05 12:55:10 +0300

    updated

--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -1,16 +1,65 @@
-import { useEffect, useState } from "react";
+/*import { useEffect, useState } from "react";
 
 function Actors() {
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        {/* What component should go here? *}
       </header>
       <main>
-        {/* Actor info here! */}
+        {/* Actor info here! *}
       </main>
     </>
   );
 };
+
+export default Actors;
+*/
+
+
+
+import NavBar from "./NavBar"; // Adjust the import path as needed
+
+const actors = [
+  {
+    name: "Benedict Cumberbatch",
+    movies: ["Doctor Strange", "The Imitation Game", "Black Mass"],
+  },
+  {
+    name: "Justin Timberlake",
+    movies: ["Trolls", "Friends with Benefits", "The Social Network"],
+  },
+  {
+    name: "Anna Kendrick",
+    movies: ["Pitch Perfect", "Into The Wood"],
+  },
+  {
+    name: "Tom Cruise",
+    movies: [
+      "Jack Reacher: Never Go Back",
+      "Mission Impossible 4",
+      "War of the Worlds",
+    ],
+  },
+];
+
+function Actors() {
+  return (
+    <>
+      <NavBar />
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <article key={index}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, idx) => (
+              <li key={idx}>{movie}</li>
+            ))}
+          </ul>
+        </article>
+      ))}
+    </>
+  );
+}
 
 export default Actors;
